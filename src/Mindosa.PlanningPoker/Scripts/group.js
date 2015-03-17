446a222ebe5b92eae2e431616e3661bd4049c59d
Joseph P. Bertles <jbertles@innovativesol.com>
2015-03-17 08:12:11 -0400

    switched over to using rivets

--- src/Mindosa.PlanningPoker/Scripts/group.js
+++ src/Mindosa.PlanningPoker/Scripts/group.js
@@ -1,188 +1,141 @@
 ﻿var group = (function () {
+    rivets.formatters.preventDefault = function (value) {
+        return function (e) {
+            e.preventDefault();
+            value.call(this, e);
+            return false;
+        };
+    };
+    
+    // Declare a proxy to reference the hub. 
+    var planningHub = $.connection.planningHub;
+    
+    function areAllCardsSelected() {
+        for (var i = 0; i < model.users.length; i++) {
+            if (!model.users[i].isObserver && !model.users[i].card.selected) {
+                return false;
+            }
+        }
 
-    function init(groupName) {
-
-        var emptyCard = '&nbsp;';
-        var planningPoker = new PokerViewModel();
-
-        // Declare a proxy to reference the hub. 
-        var planningHub = $.connection.planningHub;
-
-        planningHub.client.reset = function(group) {
-            planningPoker.cards(group.Cards);
-            planningPoker.reset(group.GroupMembers);
+        return true;
+    }
+    
+    function setCards(cards) {
+        model.cards = [];
+        model.cardList = cards.join(',');
+        
+        for (var i = 0; i < cards.length; i++) {
+            model.cards.push({
+                name: cards[i],
+                isSelected: false
+            });
+        }
+    }
+    
+    var init = function (groupName) {
+        model.group = groupName;
+        var priorUserName = $.cookie('username');
+        model.myUserName = priorUserName || '';
+        
+        planningHub.client.reset = function (group) {
+            setCards(group.Cards);
+            
+            model.users = [];
+            for (var i = 0; i < group.GroupMembers.length; i++) {
+                model.users.push({
+                    name: group.GroupMembers[i].Name,
+                    isObserver: group.GroupMembers[i].IsObserverOnly,
+                    card: {
+                        name: group.GroupMembers[i].SelectedCard,
+                        selected: group.GroupMembers[i].SelectedCard && group.GroupMembers[i].SelectedCard != ''
+                    }
+                });
+            }
+            
+            model.showSelections = areAllCardsSelected();
         };
 
         planningHub.client.cardSelected = function(username, selectedCard) {
-            planningPoker.selectCard(username, selectedCard);
+            for (var i = 0; i < model.users.length; i++) {
+                if (model.users[i].name == username) {
+                    model.users[i].card.name = selectedCard;
+                    model.users[i].card.selected = selectedCard && selectedCard != '';
+                }
+            }
+
+            model.showSelections = areAllCardsSelected();
         };
 
         planningHub.client.clear = function() {
-            planningPoker.clear();
+            for (var i = 0; i < model.users.length; i++) {
+                model.users[i].card.name = '';
+                model.users[i].card.selected = false;
+            }
+            model.myCard = null;
+            
+            for (var j = 0; j < model.cards.length; j++) {
+                model.cards[j].selected = false;
+            }
+
+            model.showSelections = false;
         };
 
         planningHub.client.updateCards = function(cards) {
-            planningPoker.clear();
-            planningPoker.cards(cards);
+            planningHub.client.clear();
+            setCards(cards);
         };
 
         // Start the connection.
         $.connection.hub.start().done(function() {
-            planningHub.server.joinGroup(groupName, planningPoker.myUserName()).done(function(userName) {
-                planningPoker.myUserName(userName);
+            planningHub.server.joinGroup(model.group, model.myUserName).done(function(userName) {
                 $.cookie('username', userName);
             });
         });
+    };
 
-        function UserModel(name, card, observerOnly) {
-            var self = this;
-            self.name = ko.observable(name);
-            self.card = ko.observable(card);
-            self.observerOnly = ko.observable(observerOnly);
-            self.isSelected = ko.computed(function() {
-                return self.card() != emptyCard;
-            });
-            self.getState = ko.computed(function() {
-                if (self.observerOnly()) {
-                    return "observer";
-                } else if (self.card() != emptyCard) {
-                    return "selected";
-                }
-                return "";
-            })
-        }
+    var model = {
+        group: '',
+        users: [],
+        cards: [],
+        myUserName: '',
+        cardList:'',
+        observerOnly: false,
+        myCard: null,
+        showSelections: false
+    };
 
-        function PokerViewModel() {
-            var self = this;
+    var viewModel = {
+        onCardClicked: function (el, args) {
+            if (!areAllCardsSelected()) {
+                planningHub.server.selectCard(model.group, model.myUserName, args.card.name).done(function (success) {
+                    if (success) {
+                        model.myCard = args.card.name;
 
-            var priorUserName = $.cookie('username');
-
-            self.users = ko.observableArray([]);
-            self.cards = ko.observableArray([]);
-            self.myUserName = ko.observable(priorUserName || '');
-            self.showSettingsPanel = ko.observable(false);
-            self.observerOnly = ko.observable(false);
-            self.myCard = ko.observable(null);
-            self.cardList = ko.computed({
-                read: function() {
-                    return self.cards();
-                },
-                write: function(value) {
-                    self.cards(value.split(','));
-                },
-                owner: this
-            });
-
-            /* hub proxies */
-            self.reset = function(groupMembers) {
-                var mappedUsers = $.map(groupMembers, function(item) {
-                    var username = $('<div />').text(item.Name).html();
-                    if (username == self.myUserName()) {
-                        self.myCard(item.SelectedCard);
-                    }
-                    return new UserModel(username, item.SelectedCard || '&nbsp;', item.IsObserverOnly);
-                });
-                self.users(mappedUsers);
-            };
-
-            self.selectCard = function(username, card) {
-                for (var i = 0; i < self.users().length; i++) {
-                    var user = self.users()[i];
-                    if (user.name() == username) {
-                        user.card(card);
-                        return;
-                    }
-                }
-            };
-            self.clear = function() {
-                for (var i = 0; i < self.users().length; i++) {
-                    var user = self.users()[i];
-                    user.card(emptyCard);
-                }
-                self.myCard(null);
-            };
-
-            /* display helpers */
-            self.areAllCardsSelected = function() {
-                for (var i = 0; i < self.users().length; i++) {
-                    var user = self.users()[i];
-                    if (!user.observerOnly() && user.card() == emptyCard) {
-                        return false;
-                    }
-                }
-                return true;
-            };
-            self.getCardValue = function(card) {
-                return self.areAllCardsSelected() ? card : emptyCard;
-            };
-            self.isSelectedCard = function(card) {
-                return self.myCard() == card;
-            };
-
-
-            /* event handlers */
-            self.onUpdateSettingsClicked = function() {
-                $.cookie('username', self.myUserName());
-                $("#lnk-settings").click();
-                planningHub.server.updateSettings(groupName, self.myUserName(), self.observerOnly(), self.cards());
-            };
-            self.onClearClicked = function() {
-                planningHub.server.clear(groupName);
-            };
-            self.onCardClicked = function(card) {
-                var originalCard = planningPoker.myCard();
-
-                if (!planningPoker.areAllCardsSelected()) {
-                    planningHub.server.selectCard(groupName, self.myUserName(), card).done(function(success) {
-                        if (success) {
-                            planningPoker.myCard(card);
-                        } else {
-                            planningPoker.myCard(originalCard);
+                        for (var i = 0; i < model.cards.length; i++) {
+                            model.cards[i].selected = model.cards[i].name == model.myCard;
                         }
-                    });
-                }
-            };
-            self.onSettingsClicked = function() {
-                self.showSettingsPanel(!self.showSettingsPanel());
-            };
-
-        }
-
-
-        ko.bindingHandlers.visible = {
-            init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                var value = ko.utils.unwrapObservable(valueAccessor());
-
-                var $element = $(element);
-
-                if (value)
-                    $element.show();
-                else
-                    $element.hide();
-            },
-            update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                var value = ko.utils.unwrapObservable(valueAccessor());
-
-                var $element = $(element);
-
-
-                var allBindings = allBindingsAccessor();
-
-                // Grab data from binding property
-                var duration = allBindings.duration || 500;
-                var isCurrentlyVisible = !(element.style.display == "none");
-
-                if (value && !isCurrentlyVisible)
-                    $element.show(duration);
-                else if ((!value) && isCurrentlyVisible)
-                    $element.hide(duration);
+                    } else {
+                        //do nothing
+                    }
+                });
             }
-        };
-
-        ko.applyBindings(planningPoker);
-    }
-
+        },
+        onClearClicked: function(el, args) {
+            planningHub.server.clear(model.group);
+        },
+        onSettingsClicked: function(el, args) {
+            $("#settings-panel").slideToggle(300);
+        },
+        onUpdateSettingsClicked: function (el, args) {
+            $.cookie('username', model.myUserName);
+            planningHub.server.updateSettings(model.group, model.myUserName, model.observerOnly, model.cardList.split(','));
+            $("#settings-panel").slideToggle(300);
+        }
+    };
+    
     return {
-      init: init  
+        model: model,
+        viewModel: viewModel,
+        init: init
     };
 })();
